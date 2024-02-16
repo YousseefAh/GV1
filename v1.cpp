a8e4b94224a4ee226703ba8eb9a304ecfd33e456
YousseefAh <yousseef.ah@gmail.com>
2024-02-16 20:45:18 +0200

    input and output of an array

--- v1.cpp
+++ v1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+
 using namespace std;
 int main()
 {
-    cout << "hello world!5";
+
+    int marks[100];
+    cout<<"how many students";
+    int numstudends;
+    cin>>numstudends;
+    cout<<"Fill the array";
+
+    for (int i=0 ;i<numstudends;i++){
+        cin>> marks[i];
+    }
+    for(int i=0;i<numstudends;i++){
+        cout<<marks[i] ;
+    }
 }
